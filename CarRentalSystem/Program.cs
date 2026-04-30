6614db8b9f0fc9a662f275c04f1ee1574e695505
Omar Zaky <zaky.omar@outlook.com>
2026-04-30 23:07:42 +0300

    commit

--- CarRentalSystem/Program.cs
+++ CarRentalSystem/Program.cs
@@ -1,6 +1,7 @@
+using CarRentalSystem.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Windows.Forms;
-using CarRentalSystem.Data;
 
 namespace CarRentalSystem
 {
@@ -14,6 +15,8 @@ namespace CarRentalSystem
             // Trigger the Database Seeder
             using (var context = new AppDbContext())
             {
+
+                context.Database.Migrate();
                 // This will safely check if data exists, and if not, inject all our dummy data
                 DatabaseSeeder.SeedData(context);
             }
