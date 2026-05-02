613c192e8427d93d0b704a5b643e3cefe3e11a30
omar887 <cleveromar00@gmail.com>
2026-05-02 05:22:09 +0300

    minor

--- CarRentalSystem/Forms/Car_View.cs
+++ CarRentalSystem/Forms/Car_View.cs
@@ -1,22 +1,47 @@
-﻿using CarRentalSystem.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using CarRentalSystem.Data;
+using CarRentalSystem.Models;
+using CarRentalSystem.Core;
 
 namespace CarRentalSystem.Forms
 {
     public partial class Car_View : Form
     {
+        private Vehicle car;
+        private DateTime dateto;
+        private DateTime datefrom;
 
-        public Car_View()
+
+        public Car_View(Vehicle carobject,DateTime from,DateTime to)
         {
             InitializeComponent();
-
+            car = carobject;
+            loadcardetails();
+            dateto = to;
+            datefrom = from;
         }
+        private void loadcardetails()
+        {
+            label6.Text = car.Id.ToString();
+            label4.Text = car.Model;
+            label9.Text = car.PlateNum;
+            label7.Text = car.Category.VehicleType;
+            label14.Text = datefrom.ToString();
+            label16.Text = dateto.ToString();
+            int  temp = (dateto - datefrom).Days;
+            decimal rate = car.Category.DailyRate;
+            rate *=temp;
+            label13.Text = rate.ToString();
+            
+            
+        }
+        
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
@@ -30,11 +55,25 @@ namespace CarRentalSystem.Forms
 
         private void button3_Click(object sender, EventArgs e)
         {
+            using ( var context = new AppDbContext())
+            {
+                var newreserves = new Reserves
+                {
+                    VehicleId = car.Id,CustomerId = UserSession.CurrentUser.Id,ReservationDate = datefrom,Duration = (dateto-datefrom).Days,Status = ReservationStatus.Reserved
+
+                };
+                context.Reservations.Add(newreserves);
+                context.SaveChanges();
+                MessageBox.Show("u have bought the car successfully!","Success",MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            this.Close();
 
         }
 
@@ -54,21 +93,6 @@ namespace CarRentalSystem.Forms
         }
 
         private void label16_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label7_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Car_View_Load(object sender, EventArgs e)
         {
 
         }
